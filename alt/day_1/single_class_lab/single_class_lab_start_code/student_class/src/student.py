190b12e0eb40a132a3a36c39d6ba48a678ea5944
Daniel FCS <danielf.cs@outlook.com>
2023-04-10 15:35:54 +0100

    Finished single part homework

--- alt/day_1/single_class_lab/single_class_lab_start_code/student_class/src/student.py
+++ alt/day_1/single_class_lab/single_class_lab_start_code/student_class/src/student.py
@@ -1,1 +1,13 @@
+class Student:
+    def __init__(self,student_name,student_cohort):
+        self.name = student_name
+        self.cohort = student_cohort
+        # self.language = favourite_language
 
+    def talk(self):
+
+        return "I can talk!"
+
+    def say_favourite_language(self,language):
+        if language == "Python":
+            return "I love Python"
