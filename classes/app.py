675c3aad556d57f38649379272d39b2ea0e27dc9
Daniel FCS <danielf.cs@outlook.com>
2023-04-10 15:18:19 +0100

    connected to github

--- classes/app.py
+++ classes/app.py
@@ -1,13 +1,25 @@
 # app.py
+from modules.bank_account import *
 
-account = {
-    "holder_name": "John",
-    "balance": 500,
-    "type": "business",
-}
+# account = {
+#     "holder_name": "John",
+#     "balance": 500,
+#     "type": "business",
+# }
 
-def get_account_name(account_name):
-    return account["holder_name"]
+# print(get_account_name(account))
 
 
-print(get_account_name(account))
+bank_account = BankAccount("John",-100,"personal")
+bank_account1 = BankAccount("Bob",302,"personal")
+bank_account.pay_in(1000)
+# print(bank_account.holder_name)
+
+# bank_account.holder_name = "Pantera"
+# print(bank_account.holder_name)
+
+# print(bank_account.balance)
+
+bank_account.pay_monthly_fee()
+print(bank_account.balance)
+
